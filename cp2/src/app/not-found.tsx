93078fab91eb0228c408a3fb720154ce0811ea02
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-10-17 21:12:35 -0300

    adicionando not-found

---
+++ cp2/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="div-page-not-found flex justify-center items-center">
+      <h1 className="h1-page-not-found md:text-custom-yellow font-bold text-xl">
+        Página não encontrada. Acesse outra página!
+      </h1>
+    </div>
+  );
+}
