d9d8deaecee3c2ab229cd4ec1e53ad9d3b9ef057
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-10-18 14:24:40 -0300

    api consumida

--- cp2/src/app/nasa/page.tsx
+++ cp2/src/app/nasa/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import Image from 'next/image';
+import { ImagemNasa } from "@/types";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Nasa() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<ImagemNasa[]>([]);
 
   const chamadaImages = async () => {
-    const response = await fetch("http://localhost:3000/api/base-nasa");
-    const dados = await response.json();
-    setData(dados);
+    try {
+      const response = await fetch("http://localhost:3000/api/base-nasa");
+      if (!response.ok) {
+        throw new Error("Falha ao obter dados");
+      }
+      const dados = await response.json();
+      setData(dados.latest_photos);
+    } catch (error) {
+      console.error("Erro ao chamar a API:", error);
+    }
   };
 
   useEffect(() => {
@@ -21,7 +29,13 @@ export default function Nasa() {
   return (
     <div>
       <h1>Fotos da API da Nasa</h1>
-      <div></div>
+      <div>
+        {data.map((imagem) => (
+          <div key={imagem.id}>
+            <p>ID: {imagem.id}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ cp2/src/types.ts
@@ -0,0 +1,12 @@
+export type ImagemNasa = {
+  id: number;
+  sol: number;
+  camera: {
+    id: number;
+    name: string;
+    rover_id: number;
+    full_name: string;
+  };
+  img_src: string;
+  earth_date: string;
+};
