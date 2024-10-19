5cbaba6e0328ba9f6c12f216eebd36fcdd922ee9
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-10-19 00:51:12 -0300

    adicionando background na home

--- cp2/src/app/page.tsx
+++ cp2/src/app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import imgBackground from "../img/background.jpg";
+
 export default function Home() {
   return (
-    <div className="home-container">
-      <h1>Home</h1>
+    <div className="home-container flex items-center justify-center bg-[url('../img/background.jpg')] bg-cover w-full h-80vh">
+      <h1 className="home-container-h1 md:text-custom-yellow font-bold font-sans text-8xl">
+        WORLDS IN COLLISION
+      </h1>
     </div>
   );
 }
